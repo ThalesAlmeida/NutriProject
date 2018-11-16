83aba75d29415998228cd8c5fa247e0b6dbfb2a5
Thales <thalesalves44@gmail.com>
2018-11-16 20:49:39 -0300

    fazendo verificação para ver se o usuário está logado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,13 +4,30 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { HomePage } from '../pages/home/home';
+import { AngularFireAuth } from 'angularfire2/auth';
+import { DicasPage } from '../pages/dicas/dicas';
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage:any = HomePage;
+  rootPage:any;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+  constructor(platform: Platform, 
+    statusBar: StatusBar,
+    splashScreen: SplashScreen, 
+    angularfireauth: AngularFireAuth) {
+
+      // Verificando se o usuário está logado
+      const authObserver = angularfireauth.authState.subscribe(users => {
+        if(users){
+          this.rootPage = DicasPage;
+          authObserver.unsubscribe();
+        }else{
+          this.rootPage = HomePage;
+          authObserver.unsubscribe();
+        }
+      })
+
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
